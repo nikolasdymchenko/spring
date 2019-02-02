d35c31341e1c75e2ad9715b8e591e3ae934182bc
Mikolaj Dymchenko <nikolasdymchenko@gmail.com>
2019-02-02 19:06:24 +0100

    Spring. Lesson 7. Uploading files to the server and distributing statics

--- src/main/java/com/example/sweater/domain/Message.java
+++ src/main/java/com/example/sweater/domain/Message.java
@@ -19,6 +19,8 @@ public class Message {
     @JoinColumn(name = "user_id") // made in the column  in out field had a name user_id and not the author of the standard.
     private User author;
 
+    private String filename;
+
     public Message() {
     }
 
@@ -58,5 +60,13 @@ public class Message {
 
     public void setTag(String tag) {
         this.tag = tag;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
     }
 }
